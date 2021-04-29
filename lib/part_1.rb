38cf26a61aaa7e32d04fd61b8b0970bbd3698e9b
Zane Vandergriff <zanevande@gmail.com>
2021-04-29 19:58:51 -0400

    Blocks Lesson

---
+++ lib/part_1.rb
@@ -0,0 +1,19 @@
+def select_even_nums(arr)
+    arr.select(&:even?) 
+end
+
+def reject_puppies(arr)
+    arr.reject { |age| "age" >= 2 }
+end
+
+def count_positive_subarrays(arr)
+
+end
+
+def aba_translate(str)
+
+end
+
+def aba_array(arr)
+
+end
