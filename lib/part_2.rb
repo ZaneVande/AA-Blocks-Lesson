7d8a7aebc81d420dffa5543378cc30bf76b73181
Zane Vandergriff <zanevande@gmail.com>
2021-04-29 21:45:04 -0400

    Finished Part2 of 2 Block Exercises

--- lib/part_2.rb
+++ lib/part_2.rb
@@ -1,11 +1,18 @@
 def all_words_capitalized?(arr)
-    
+    arr.all? { |word| word.capitalize == word }
 end
 
 def no_valid_url?(arr)
-
+    valid = ['.com', '.net', '.io', '.org']
+    arr.none? do |url|
+        valid.any? { |ending| url.end_with?(ending) }
+    end
 end
 
 def any_passing_students?(arr)
-
+    arr.any? do |student|
+        grades = student[:grades]
+        avg = grades.sum / (grades.length * 1.0)
+        avg >= 75
+    end
 end
